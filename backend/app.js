b84cc9b68f8408077aadd9c19c28dd6fce76fe04
Takács Marcell <tmarcell20012001@gmail.com>
2025-03-18 14:41:15 +0100

    backend létrehozása

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+const cors = require("cors");
+
+dotenv.config();
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
